0083f16ffe8ea49fae3a447bef92d655acebed04
Pradeep Yadav <104863759+Pradeep9651@users.noreply.github.com>
2023-09-15 15:34:57 +0530

    Rangoli using python

---
+++ Untitled.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import turtle
+t=turtle.Pen()
+colors=["yellow","red","green","purple"]
+turtle.bgcolor("black")
+for x in range(450):
+  t.pencolor(colors[x%4])
+  t.forward(x)
+  t.left(150)
+
+
+# In[1]:
+
+
+2+4
+
+
+# In[ ]:
+
+
+
+
